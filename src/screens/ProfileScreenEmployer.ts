4693512ca2668b679bee96a5b5273c2dd5ba503f
Shaun Ruddy <93016289+ShaunRuddy@users.noreply.github.com>
2021-11-05 18:11:21 -0400

    Create ProfileScreenEmployer.ts

---
+++ src/screens/ProfileScreenEmployer.ts
@@ -0,0 +1,15 @@
+import { Button } from "native-base";
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { auth } from "../config";
+
+export const ProfileScreen = () => {
+	
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: "#F5FCFF",
+	},
+});
